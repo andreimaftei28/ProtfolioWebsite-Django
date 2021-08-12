06beefea49b02c5ed1cdea51873dad8000858cf4
Andrei Claudiu Maftei <andrei.maftei28@gmail.com>
2021-08-12 23:38:42 +0200

    First commit

---
+++ jobs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    """docstring for Job."""
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    summary = models.TextField(max_length=400)
+    link = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.title
