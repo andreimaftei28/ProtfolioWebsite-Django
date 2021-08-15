530a69a7c19a0b83610b4264c09e3020813e761a
Andrei Claudiu Maftei <andrei.maftei28@gmail.com>
2021-08-15 11:47:12 +0200

    Change media location

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     """docstring for Job."""
-    image = models.ImageField(upload_to='media/images/')
+    image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=100)
     summary = models.TextField(max_length=400)
     link = models.CharField(max_length=200)
